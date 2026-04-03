d1b3743a2c541a4fecb905f44ea36c00a436347f
abhishek kumar <abhishek756491@gmail.com>
2026-04-03 15:38:48 +0530

    update2.30

--- test2.js
+++ test2.js
@@ -1,72 +1,116 @@
-// Q1
-let s = "Javascript"
-//print total number of characters
-console.log(s.length);
+// // Q1
+// let s = "Javascript"
+// //print total number of characters
+// console.log(s.length);
 
-//print script from the same string
-console.log(s.slice(4,10))
+// //print script from the same string
+// console.log(s.slice(4,10))
 
-//print full except last word
-console.log(s.slice(0,9))
+// //print full except last word
+// console.log(s.slice(0,9))
 
-//Q2
-let nums = [10,20,30,40,50]
+// //Q2
+// let nums = [10,20,30,40,50]
 
-//remove the first value
-let val = nums.shift()
-console.log(nums)
+// //remove the first value
+// let val = nums.shift()
+// console.log(nums)
 
-let numb = [10,20,30,40,50]
-//Add new value at the beginning
-let Add = numb.unshift(5)
-console.log(numb)
+// let numb = [10,20,30,40,50]
+// //Add new value at the beginning
+// let Add = numb.unshift(5)
+// console.log(numb)
 
-let number = [10,20,30,40,50]
-//Extract only second value
-let changeNum = number.slice(1,3)
-console.log(changeNum)
+// let number = [10,20,30,40,50]
+// //Extract only second value
+// let changeNum = number.slice(1,3)
+// console.log(changeNum)
 
-//DElet the the third value from the updted array
-let del3 = number.splice(2,1)
-console.log(number)
+// //DElet the the third value from the updted array
+// let del3 = number.splice(2,1)
+// console.log(number)
 
-//q3. Using for loop and if-else,print numbers from 1 to 30
-//-if number is even ->print"Even"
-//-if number is odd ->print"Odd"
-for(i=1; i<=30; i++){
-    if(i/2 !== 0 )
-    console.log("Odd")
-    else(i/2 === 0 ) 
-    console.log("Even")
-}
+// //q3. Using for loop and if-else,print numbers from 1 to 30
+// //-if number is even ->print"Even"
+// //-if number is odd ->print"Odd"
+// for(i=1; i<=30; i++){
+//     if(i/2 !== 0 )
+//     console.log("Odd")
+//     else(i/2 === 0 ) 
+//     console.log("Even")
+// }
 
-//Create a button with the label "Show Image". When clicked: If the image is
-//hidden, show it and change button text to "Hide Image
-function toggleImage() {
-            // Get references to the image and button elements
-            var image = document.getElementById("myImage");
-            var button = document.getElementById("toggleButton");
+// //Create a button with the label "Show Image". When clicked: If the image is
+// //hidden, show it and change button text to "Hide Image
+// function toggleImage() {
+//             // Get references to the image and button elements
+//             var image = document.getElementById("myImage");
+//             var button = document.getElementById("toggleButton");
 
-            // Check the current display style property of the image
-            if (image.style.display === "none") {
-                // If hidden, show the image and change button text to "Hide Image"
-                image.style.display = "block";
-                button.textContent = "Hide Image";
-            } else {
-                // If visible, hide the image and change button text to "Show Image"
-                image.style.display = "none";
-                button.textContent = "Show Image";
-            }
-        }
+//             // Check the current display style property of the image
+//             if (image.style.display === "none") {
+//                 // If hidden, show the image and change button text to "Hide Image"
+//                 image.style.display = "block";
+//                 button.textContent = "Hide Image";
+//             } else {
+//                 // If visible, hide the image and change button text to "Show Image"
+//                 image.style.display = "none";
+//                 button.textContent = "Show Image";
+//             }
+//         }
 
 //method Write a JavaScript program to reverse an array manually (using loop
 //  logic only, not using reverse() method). Example: Input [1, 2, 3, 4]
 //  Output [4, 3, 2, 1
 
 
- arr = [1,2,3,4]
- arr1 = []
-for(let i=arr.length-1; i >= 0; i--){
-    arr1.push(arr[i]);
+//  arr = [1,2,3,4]
+//  arr1 = []
+// for(let i=arr.length-1; i >= 0; i--){
+//     arr1.push(arr[i]);
+// }
+// console.log(arr1)
+
+
+
+//1. Even or Odd Checker
+
+let even=()=>{
+    let number=prompt("Enter your choice number")
+    if(number%2)
+        alert("Even")
+    else
+        alert("Odd")
 }
-console.log(arr1)
+
+//2. Positive, Negative Number or zero
+
+let math=()=>{
+let num=prompt("Enter your choice number")
+if (num>0)
+    alert("positive number")
+else
+    alert("0")
+if(num<0)
+    alert("negative number")
+
+}
+
+console.log(math());
+
+//
+
+
+//1. Age Checker (Voting Eligibility)
+
+let vote=()=>{
+    let i=prompt("Enter your age for vote")
+    
+    if ( i>=18)
+        alert("you can eligible for vote")
+    else
+        alert("you can eligible for vote")
+}
+
+//largest of two
+
