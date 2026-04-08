66be4d566b00c408b2549f2acf45c48e9cd87a71
abhishek kumar <abhishek756491@gmail.com>
2026-04-08 20:44:53 +0530

    update08/04

--- class.js
+++ class.js
@@ -1,58 +1,121 @@
 
-// function btn(){
-//     let writearea=document.querySelector("#write").value;
-//    let print=document.querySelector("#print");
+function btn(){
+    let writearea=document.querySelector("#write").value;
+   let print=document.querySelector("#print");
 
-//     let area=writearea.toUpperCase();
-//     print.innerText=area;
-// }
+    let area=writearea.toUpperCase();
+    print.innerText=area;
+}
 
-// function btntolower(){
-//     let writearea=document.querySelector("#write").value;
-//    let print=document.querySelector("#print");
+function btntolower(){
+    let writearea=document.querySelector("#write").value;
+   let print=document.querySelector("#print");
 
-//     let area=writearea.toLowerCase();
-//     print.innerText=area;
-// }
+    let area=writearea.toLowerCase();
+    print.innerText=area;
+}
 
-// function btnlength(){
-//     let writearea=document.querySelector("#write").value;
-//    let print=document.querySelector("#print");
+function btnlength(){
+    let writearea=document.querySelector("#write").value;
+   let print=document.querySelector("#print");
 
-//     let area=writearea.length;
-//     print.innerText=area;
-// }
+    let area=writearea.length;
+    print.innerText=area;
+}
 
-// function btnreverse(){
-//     let writearea=document.querySelector("#write").value;
-//    let print=document.querySelector("#print");
+function btnreverse(){
+    let writearea=document.querySelector("#write").value;
+   let print=document.querySelector("#print");
 
-//     let reversedstr="";
-//    for(let i=writearea.length-1;i>=0;i--){
-//        reversedstr+=writearea[i]
+    let reversedstr="";
+   for(let i=writearea.length-1;i>=0;i--){
+       reversedstr+=writearea[i]
     
-//     print.innerHTML=reversedstr;
-//     }
-// }
+    print.innerHTML=reversedstr;
+    }
+}
 
-// function btncount(){
-//     let writearea=document.querySelector("#write").value;
-//    let print=document.querySelector("#print");
+function btncountvowel(){
+    let writearea=document.querySelector("#write").value;
+   let print=document.querySelector("#print");
 
-//    let count=0;
-//    for(let i=0;i<=writearea.length;i++)
-//    {
-//     let char=writearea[i];
+   let count=0;
+   for(let i=0;i<writearea.length;i++)
+   {
+    let char=writearea[i];
   
-//     if(char === 'a' || char === 'e'  || char === 'i'  || char === 'o'  || char === 'u' 
-//          || char === 'A'  || char === 'E'  || char === 'I'  || char === 'O'  || char === 'U' )
+    if(char === 'a' || char === 'e'  || char === 'i'  || char === 'o'  || char === 'u' 
+         || char === 'A'  || char === 'E'  || char === 'I'  || char === 'O'  || char === 'U' )
       
-//        count++;
-//      }
-//     print.innerHTML=count;
-//     }
+       count++;
+     }
+    print.innerHTML=count;
+    }
 
-let EvenNodd=()=>{
+    function  pelindrome(){
+     let str=prompt("Enter")
+
+     let reversestr="";
+
+     for(let i=str.length-1;i>=0;i--)
+     {
+        reversestr +=str[i];
+    }
+        if(str.toLowerCase() === reversestr.toLowerCase())
+            alert(str+" it is pelindrome")
+        else
+            alert(str+" it is not pelindrome")
+     
+    }
+    // console.log(pelindrome())
+
+    let btnremove=()=>{
+        let writearea=document.querySelector("#write").value;
+        let print=document.querySelector("#print");
+
+        let str="";
+        for(let i=0;i<writearea.length;i++)
+        {
+            if(writearea[i] !== " "){
+            str+=writearea[i];
+            }
+        }
+        print.innerHTML=str;
+    }
+
+    function excespace(){
+    let writearea=document.querySelector("#write").value;
+   let print=document.querySelector("#print");
+
+   let count=0;
+   for(let i=0;i<writearea.length;i++)
+   {
+    let char=writearea[i];
+  
+    if(char !== " ")
+       count++;
+     }
+    print.innerHTML=count;
+    }
+
+    let btnfirst=()=>{
+         let writearea=document.querySelector("#write").value;
+         let print=document.querySelector("#print");
+      
+             let result=writearea.charAt(0).toUpperCase()+writearea.slice(1);
+             print.innerHTML=result;
+            
+    }
+
+    
+     let countword=()=>{
+         let writearea=document.querySelector("#write").value.trim();
+         let print=document.querySelector("#print");
+      
+      let count=0
+    
+    }
+    let EvenNodd=()=>{
     let n=prompt("Enter our number")
     if(n%2===0)
         alert("Even")
@@ -119,5 +182,5 @@ let even=()=>{
          console.log(+count)
     }
 }
-console.log(even())
+// console.log(even())
 
