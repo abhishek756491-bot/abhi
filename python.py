1b2dbe87b249658658c4ae899642c44802e1108a
abhishek kumar <abhishek756491@gmail.com>
2026-04-19 19:55:42 +0530

    python

---
+++ python.py
@@ -0,0 +1,43 @@
+# a=input("Enter your name")
+# print(len(a))
+
+#count doller in strin
+# str="njkin $lkmn$ kjnfdn$ $KJnin $"
+# print(str.count("$"))
+
+# t=(input("three favorite movies name :"))
+# tup=[t]
+# print(tup)
+
+# print(bin(15))
+# x =[1,256]   
+# b = bytes(x)  
+# print(type(b))
+
+# d={101:'durga',102:'ravi',103:'shiva'} 
+# d["abhi"]=35
+# d[52]='banana'
+# print(d) 
+
+# print("abhishek\rkumar")
+
+# print("durga" and "durgasoft")
+# print(~5)
+
+# a=int(input("Enter First Number:"))
+# b=int(input("Enter Second Number:"))
+# c=int(input("Enter Third Number:"))
+# min=a if a<b and a<c else b if b<c else c
+# print("Minimum Value:",min) 
+
+# a=int(input("Enter First Number:"))
+# b=int(input("Enter Second Number:"))
+# c=int(input("Enter Third Number:"))
+# max=a if a>b and a>c else b if b>c else c
+# print("Maximum Value:",max) 
+
+# a=int(input("Enter First Number:"))
+# b=int(input("Enter Second Number:"))
+# print("Both numbers are equal" if a==b else "First Number is Less than Second Number" if
+# a<b else "First Number Greater than Second Number")
+
