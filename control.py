da9be8d1ed15cff7637a34e6ce60499819a0c762
abhishek kumar <abhishek756491@gmail.com>
2026-04-22 20:26:54 +0530

    update22/04

---
+++ control.py
@@ -0,0 +1,53 @@
+# only if
+# name=input("Enter Name:")
+# if name=="abhi" :
+#    print("Hello Durga Good Morning")
+#    print("How are you!!!") 
+
+#if else
+# name=input("Enter Name:")
+# if name=="abhi" :
+#    print("Hello Durga Good Morning")
+# else:
+#    print("Hello Guest Good Moring")
+#    print("How are you!!!")
+
+""" for x in range(10) :
+ print("Hello") 
+
+for x in range(11) :
+ print(x) """
+
+# for x in range(10,0,-1) :
+#  print(x)
+
+# list=eval(input("Enter List:"))
+# sum=0
+# for x in list:
+#  sum=sum+x
+#  print("The Sum=",sum)
+
+# name=""
+# while name!="abhi":
+#    name=input("Enter Name:")
+# print("Thanks for confirmation")
+
+
+"""1)*
+2) * *
+3) * * *
+4) * * * *
+5) * * * * *
+6) * * * * * *
+7) * * * * * * *"""
+
+# for i in range(6):
+#  for j in range(i+1):
+#        print("*",end=" ") 
+#  print()
+
+for i in range(10):
+  if i==7:
+    print("processing is enough..plz break")
+    break
+  print(i)
---
+++ string.py
@@ -0,0 +1,7 @@
+name=input("Enter your name :")
+i=0
+n=len(name)
+for p in name:
+  print("index posit is {} and negetive is  -{} charater = {}".format(i,n,p))
+  i=i+1
+  n=n-1
