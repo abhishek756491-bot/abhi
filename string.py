bf72dd99896c652376544556d7ee5d6254e73a58
abhishek kumar <abhishek756491@gmail.com>
2026-04-24 13:22:36 +0530

    update24/04

--- string.py
+++ string.py
@@ -1,7 +1,27 @@
-name=input("Enter your name :")
-i=0
-n=len(name)
-for p in name:
-  print("index posit is {} and negetive is  -{} charater = {}".format(i,n,p))
-  i=i+1
-  n=n-1
+# name=input("Enter your name :")
+# i=0
+# n=len(name)
+# for p in name:
+#   print("index posit is {} and negetive is  -{} charater = {}".format(i,n,p))
+#   i=i+1
+#   n=n-1
+
+# Q. Write a program to access each character of string in forward and backward direction
+# by using while loop
+
+
+
+s = "Learning Python is very easy !!!"
+n = len(s)
+i = 0
+
+print("Forward direction")
+while i < n:
+    print(s[i], end=' ')  # Indentation zaroori hai
+    i = i + 1
+
+print("\nBackward direction") # \n se nayi line shuru hogi
+i = -1
+while i >= -n:
+    print(s[i], end=' ')  # Indentation zaroori hai
+    i = i - 1
