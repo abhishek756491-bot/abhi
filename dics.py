d4c65596b646d8ac5b2404387471264cc18f2b6e
abhishek kumar <abhishek756491@gmail.com>
2026-05-02 22:17:44 +0530

    update02/04

--- dics.py
+++ dics.py
@@ -58,3 +58,139 @@ A={"a":10,"B":20,"C":30}
 B={"B":5,"C":15,"D":25}
 A.sum(B)
 print(A)
+
+
+#second quetion
+
+person={"name":"chetan","address":{"city":"Mumbai","Zip":"460001"}}
+result=person["address"]["city"]
+print("city : ",result)
+
+dict1={"a":1,"b":2}
+dict2={"b":3,"c":4}
+dict1.update(dict2)
+print(dict1)
+
+fruits={"banana":100,"apple":30,"year":2022,"colour":"blue"}
+fruits.clear()
+print(fruits)
+
+key=["name","age","city"]
+value=["ravi",25,"ujjain"]
+result=dict(zip(key,value))
+print(result)
+
+car={"brand":"toyota","model":"carmy","year":2022,"colour":"blue"}
+car.pop("year")
+print("model Exist :","year" in car)
+print("model Exist :","colour" in car)
+
+dics = {"name":"Aman","age":"22","Grade":"B"}
+dics["age"]=25
+dics.update({ "city": "New York"})
+print(dics)
+# dc={"Name":"Amit","Age":20,"city":"bhopal"}
+dc={"Name":["Amit","murshid"], "Age":[20,45], "city":["bhopal","muna"]}
+dc
+for k,v in dc.items():
+    print(k,"=>",v)
+
+
+##########################################################
+
+q1 Expected Output: {"name": "Alice", "age": 21, "grade": "B", "city": "New York"}
+
+dics: student = {"name": "Alice", "age": 20, "grade": "B"}
+dics["age"]=21
+dics.update({ "city": "New York"})
+print(dics)
+
+x= ["name", "age", "city"]
+y= ["Bob", 25, "London"]
+#zip se ek ek value apne sthan par achala jatat hia
+print(dict(zip(x,y)))
+
+inventory = {"apples": 10, "bananas": 5, "oranges": 8}
+inventory.clear()#clear ka matlav dict empty ho jata hai
+print(inventory)
+
+dict1 = {"a": 1, "b": 2} 
+dict2 = {"b": 3, "c": 4}
+dict1.update(dict2)
+print(dict1)
+
+person = {"name": "Carol", "address": {"city": "Paris", "zip": "75001"}}
+city=person["address"]["city"]
+print("city:",city)
+
+student = {"name": "Dave", "grades": {"math": 88, "science": 92, "history": 75}}
+history=student["grades"]["history"]
+print("history grade:",history)
+
+keys = ["math", "science", "english", "history"]
+default = 0
+print(dict.fromkeys(keys,default))
+
+employee = {"fname": "John", "age": 30, "dept": "Engineering"}
+# employee."full_name"
+print(employee)
+
+d=eval(input("Enter dictionary:"))
+s=sum(d.values())
+print("Sum= ",s)
+
+word=input("Enter any word: ")
+d={}
+for x in word:
+   d[x]=d.get(x,0)+1
+for k,v in d.items():
+  print(k,"occurred ",v," times")
+
+n=int(input("Enter the number of students: "))
+d={}
+for i in range(n):
+ name=input("Enter Student Name: ")
+ marks=input("Enter Student Marks: ")
+ d[name]=marks
+ print(name,marks)
+
+A={"a":10,"B":20,"C":30}
+B={"B":5,"C":15,"D":25}
+A.sum(B)
+print(A)
+
+
+#second quetion
+
+person={"name":"chetan","address":{"city":"Mumbai","Zip":"460001"}}
+result=person["address"]["city"]
+print("city : ",result)
+
+dict1={"a":1,"b":2}
+dict2={"b":3,"c":4}
+dict1.update(dict2)
+print(dict1)
+
+fruits={"banana":100,"apple":30,"year":2022,"colour":"blue"}
+fruits.clear()
+print(fruits)
+
+key=["name","age","city"]
+value=["ravi",25,"ujjain"]
+result=dict(zip(key,value))
+print(result)
+
+car={"brand":"toyota","model":"carmy","year":2022,"colour":"blue"}
+car.pop("year")
+print("model Exist :","year" in car)
+print("model Exist :","colour" in car)
+
+dics = {"name":"Aman","age":"22","Grade":"B"}
+dics["age"]=25
+dics.update({ "city": "New York"})
+print(dics)
+# dc={"Name":"Amit","Age":20,"city":"bhopal"}
+dc={"Name":["Amit","murshid"], "Age":[20,45], "city":["bhopal","muna"]}
+dc
+for k,v in dc.items():
+    print(k,"=>",v)
